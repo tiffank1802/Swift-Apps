b74639f1283ce7aa250ed2c2c60f7556d1519d15
tiffank1802 <tiffank1802@gmail.com>
2025-07-11 14:39:58 +0200

    the forecast app

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by tongue kevin on 10/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! TONGUE KEVIN")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by tongue kevin on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
